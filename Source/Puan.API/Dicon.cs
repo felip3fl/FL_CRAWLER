1d235f4727dfe3a44bd178e9eb515507d69b54c1
FL <felip3.fl@gmail.com>
2024-03-13 20:25:02 -0300

    Update code

--- Source/Puan.API/Dicon.cs
+++ Source/Puan.API/Dicon.cs
@@ -19,51 +19,58 @@ namespace Puan.API
 
         public async Task loginAsync()
         {
-            HttpClient _httpClient = new HttpClient();
+            try
+            {
+                HttpClient _httpClient = new HttpClient();
 
-            //var responde = await _httpClient.GetAsync(urlOriginal);
-            //var cookieRaw = responde.Headers?.GetValues("Set-Cookie").FirstOrDefault();
-            //var cookie = cookieRaw.Substring(0, cookieRaw.IndexOf(';'));
+                var responde = await _httpClient.GetAsync(urlOriginal);
+                var cookieRaw = responde.Headers?.GetValues("Set-Cookie").FirstOrDefault();
+                var cookie = cookieRaw.Substring(0, cookieRaw.IndexOf(';'));
 
-            //_httpClient.DefaultRequestHeaders.set = "";
+                //_httpClient.DefaultRequestHeaders.set = "";
 
-            //_httpClient.DefaultRequestHeaders.Add("Cookie", cookie);
+                _httpClient.DefaultRequestHeaders.Add("Cookie", cookie);
 
-            var parametro = new Dictionary<string, string>();
+                var parametro = new Dictionary<string, string>();
 
-            parametro.Add("deviceID", "8001");
-            parametro.Add("eventType", "1");
-            parametro.Add("userName", "1234564");
-            parametro.Add("password", "345");
-            parametro.Add("cracha", "");
-            parametro.Add("costCenter", "");
-            parametro.Add("leave", "");
-            parametro.Add("func", "0");
-            parametro.Add("captcha", "");
-            parametro.Add("tsc", "");
-            parametro.Add("sessionID", "0");
-            parametro.Add("selectedEmployee", "0");
-            parametro.Add("selectedCandidate", "0");
-            parametro.Add("selectedVacancy", "0");
-            parametro.Add("dtFmt", "d/m/Y");
-            parametro.Add("tmFmt", "H:i:s");
-            parametro.Add("shTmFmt", "H:i");
-            parametro.Add("dtTmFmt", "d/m/Y H:i:s");
-            parametro.Add("language", "0");
-            parametro.Add("idEmployeeLogged", "0");
+                parametro.Add("deviceID", "8001");
+                parametro.Add("eventType", "1");
+                parametro.Add("userName", "1234564");
+                parametro.Add("password", "34554354");
+                parametro.Add("cracha", "");
+                parametro.Add("costCenter", "");
+                parametro.Add("leave", "");
+                parametro.Add("func", "0");
+                parametro.Add("captcha", "");
+                parametro.Add("tsc", "");
+                parametro.Add("sessionID", "0");
+                parametro.Add("selectedEmployee", "0");
+                parametro.Add("selectedCandidate", "0");
+                parametro.Add("selectedVacancy", "0");
+                parametro.Add("dtFmt", "d/m/Y");
+                parametro.Add("tmFmt", "H:i:s");
+                parametro.Add("shTmFmt", "H:i");
+                parametro.Add("dtTmFmt", "d/m/Y H:i:s");
+                parametro.Add("language", "0");
+                parametro.Add("idEmployeeLogged", "0");
 
-            var content = new FormUrlEncodedContent(parametro);
-            var request = new HttpRequestMessage();
+                var content = new FormUrlEncodedContent(parametro);
+                var request = new HttpRequestMessage();
 
-            request.Method = HttpMethod.Post;
-            request.RequestUri = new Uri(url);
-            request.Content = content;
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri(url);
+                request.Content = content;
 
-            var response = await _httpClient.SendAsync(request);
-            var conteudoPagina = await response.Content.ReadAsStringAsync();
+                var response = await _httpClient.SendAsync(request);
+                var conteudoPagina = await response.Content.ReadAsStringAsync();
 
-            HtmlDocument html = new HtmlDocument();
-            html.LoadHtml(conteudoPagina);
+                HtmlDocument html = new HtmlDocument();
+                html.LoadHtml(conteudoPagina);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
 
         }
 
